c92e38386415ffd128e4e344bae6942804cdcf0b
Sebastian Lindgren <sebastian.lindgrenen@gmail.com>
2025-01-20 16:59:11 +0100

    Added coordinate system conversion formulas

--- src/include/direction.h
+++ src/include/direction.h
@@ -1,38 +1,23 @@
+#include <glm/glm.hpp>
+#include <cmath>
+#include <algorithm>
+#include <corecrt_math_defines.h>
 
-
-class LocalDirection {
+class Direction {
 public:
-    LocalDirection(double theta, double pi) : theta(theta), phi(phi) {}
 
-    static LocalDirection GetRandomLocalDirection() {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        static std::uniform_real_distribution<> dis(0, 1);
+    Direction(double azimuth, double inclination);
+    void setDirection(double azimuth, double inclination);
+    double getAzimuth() const;
+    double getInclination() const;
+    glm::dvec3 getDirection() const;
+    glm::dvec3 HemisphericalToCartesianLocalSystem(Direction* direction); // lecture 2 DONE
+    glm::dvec3 CartesianLocalSystemToCartesianWorldSystem(glm::dvec3 localSystem, const glm::dvec3& surfaceNormal); // lecture 2 DONE
+    Direction CartesianWorldSystemToHemispherical(glm::dvec3 worldSystem); // lecture 2
+    static Direction RandomDirection(); // using Bidirectional Reflectance Distribution Function (BRDF) // lecture 3
 
-        double u = dis(gen);
-        double v = dis(gen);
-
-        double theta = 2 * M_PI * u;
-        double phi = acos(2 * v - 1);
-
-        return LocalDirection(theta, phi);
-    }
-
-    static glm::dvec3 HemisphericalToCartesian(const LocalDirection& localDir) {
-        return glm::dvec3(
-            sin(localDir.phi) * cos(localDir.theta),
-            sin(localDir.phi) * sin(localDir.theta),
-            cos(localDir.phi)
-        );
-    }
-
-    static glm::dvec3 LocalCartesianToWorldCartesian(const glm::dvec3& localDir, const glm::dvec3& normal) {
-        glm::dvec3 tangent = glm::normalize(glm::cross(normal, glm::dvec3(0.0, 0.0, 1.0)));
-        glm::dvec3 bitangent = glm::normalize(glm::cross(normal, tangent));
-
-        return localDir.x * tangent + localDir.y * bitangent + localDir.z * normal;
-    }
-
-    double theta;
-    double phi;
+private:
+    double azimuth = 0.0; // lecture 1
+    double inclination = 0.0; // lecture 1
+    glm::dvec3 directionVector; 
 };
